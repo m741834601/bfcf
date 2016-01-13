6fcf2324f1f213d7bf48a6a57ec8554a4930eccf
RenMinghe <m741834601@163.com>
2016-01-13 11:35:06 +0800

    初始化

---
+++ piapia/piapia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  piapia
+//
+//  Created by 任茗赫 on 16/1/13.
+//  Copyright © 2016年 任茗赫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ piapia/piapia/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  piapia
+//
+//  Created by 任茗赫 on 16/1/13.
+//  Copyright © 2016年 任茗赫. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
